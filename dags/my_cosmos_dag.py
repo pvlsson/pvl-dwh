91056d7a7eabb12413a6631c9afc5cd6fa1ddecb
Pavel Kukushkin <pkukushkin@outlook.com>
2025-03-23 16:30:59 +0100

    Move my dbt project

---
+++ dags/my_cosmos_dag.py
@@ -0,0 +1,32 @@
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+
+import os
+from datetime import datetime
+
+airflow_home = os.environ["AIRFLOW_HOME"]
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="airflow_db",
+        profile_args={"schema": "public"},
+    ),
+)
+
+my_cosmos_dag = DbtDag(
+    project_config=ProjectConfig(
+        f"{airflow_home}/dags/pvl-dwh",
+    ),
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{airflow_home}/dbt_venv/bin/dbt",
+    ),
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="my_cosmos_dag",
+    default_args={"retries": 2},
+)
